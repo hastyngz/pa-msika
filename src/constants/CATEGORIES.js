79dceced51a699e39e621caecec6cc5d89cc8a36
hastyngz <hastingszidanamot@gmail.com>
2025-09-07 16:17:54 +0300

    Create CATEGORIES.js

---
+++ src/constants/CATEGORIES.js
@@ -0,0 +1,7 @@
+export const CATEGORIES = [
+  { key: 'house', label: 'House for rent' },
+  { key: 'plot', label: 'Plot for sale' },
+  { key: 'clothes', label: 'Clothes for sale' },
+  { key: 'services', label: 'Services for hire' }
+];
+
